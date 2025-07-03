951298c9ef66973e956b1c822ee7c67cc565e575
Sonu Kumar <sonushivkr@gmail.com>
2025-07-03 06:13:22 +0530

    first commit

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -6,7 +6,11 @@ class ApiService {
   static const String _baseUrl = "https://jsonplaceholder.typicode.com";
 
   static Future<List<User>> fetchUsers() async {
-    final response = await http.get(Uri.parse("$_baseUrl/users"));
+    // final response = await http.get(Uri.parse("$_baseUrl/users"));
+    final response = await http.get(
+      Uri.parse("https://jsonplaceholder.typicode.com/users"),
+      headers: {"Accept": "application/json"},
+    );
     if (response.statusCode == 200) {
       List<dynamic> data = json.decode(response.body);
       return data.map((json) => User.fromJson(json)).toList();
